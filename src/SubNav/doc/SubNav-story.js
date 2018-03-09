d086cfa694d55d1331df67ada6a0d8154c1f0363
Cody Lawson <CLawson@esri.com>
2018-03-09 16:41:41 -0700

    Break subnav with `backgroundImage` into its own story

--- src/SubNav/doc/SubNav-story.js
+++ src/SubNav/doc/SubNav-story.js
@@ -13,79 +13,87 @@ import SubNav, {
   SubNavActions
 } from '../';
 
-storiesOf('SubNav', module).add(
-  'Basic',
-  withInfo(doc)(() => (
-    <div>
-      <GuideExample>
-        <SubNav>
-          <SubNavTitle href="#">Fields</SubNavTitle>
-          <SubNavList>
-            <SubNavLink active href="#">
-              Glens
-            </SubNavLink>
-            <SubNavLink href="#">Dales</SubNavLink>
-            <SubNavLink href="#">Meadows</SubNavLink>
-          </SubNavList>
-        </SubNav>
-      </GuideExample>
-      <GuideExample label="blue">
-        <SubNav blue>
-          <SubNavTitle href="#">Fields</SubNavTitle>
-          <SubNavList>
-            <SubNavLink active href="#">
-              Glens
-            </SubNavLink>
-            <SubNavLink href="#">Dales</SubNavLink>
-            <SubNavLink href="#">Meadows</SubNavLink>
-          </SubNavList>
-        </SubNav>
-      </GuideExample>
-      <GuideExample label="with actions">
-        <SubNav blue>
-          <SubNavTitle href="#">Fields</SubNavTitle>
-          <SubNavList>
-            <SubNavLink active href="#">
-              Glens
-            </SubNavLink>
-            <SubNavLink href="#">Dales</SubNavLink>
-            <SubNavLink href="#">Meadows</SubNavLink>
-          </SubNavList>
-          <SubNavActions>
-            <Button white small>
-              New Field
-            </Button>
-          </SubNavActions>
-        </SubNav>
-      </GuideExample>
-      <GuideExample label="no nav - with actions">
-        <SubNav blue>
-          <SubNavTitle href="#">Fields</SubNavTitle>
-          <SubNavActions>
-            <Button white small>
-              New Field
-            </Button>
-          </SubNavActions>
-        </SubNav>
-      </GuideExample>
-      <GuideExample label="backgroundImage">
-        <SubNav
-          backgroundImage={greenBlueBg}
-          gradientFromColor="#70be49"
-          gradientToColor="#1688aa"
-        >
-          <SubNavTitle href="#" style={{ color: '#fff' }}>
-            Fields
-          </SubNavTitle>
-          <SubNavList>
-            <SubNavLink active href="#">
-              Glens
-            </SubNavLink>
-            <SubNavLink href="#">Dales</SubNavLink>
-            <SubNavLink href="#">Meadows</SubNavLink>
-          </SubNavList>
-        </SubNav>
-      </GuideExample>
-    </div>
-  ))
-);
+storiesOf('SubNav', module)
+  .add(
+    'Basic',
+    withInfo(doc)(() => (
+      <div>
+        <GuideExample>
+          <SubNav>
+            <SubNavTitle href="#">Fields</SubNavTitle>
+            <SubNavList>
+              <SubNavLink active href="#">
+                Glens
+              </SubNavLink>
+              <SubNavLink href="#">Dales</SubNavLink>
+              <SubNavLink href="#">Meadows</SubNavLink>
+            </SubNavList>
+          </SubNav>
+        </GuideExample>
+        <GuideExample label="blue">
+          <SubNav blue>
+            <SubNavTitle href="#">Fields</SubNavTitle>
+            <SubNavList>
+              <SubNavLink active href="#">
+                Glens
+              </SubNavLink>
+              <SubNavLink href="#">Dales</SubNavLink>
+              <SubNavLink href="#">Meadows</SubNavLink>
+            </SubNavList>
+          </SubNav>
+        </GuideExample>
+        <GuideExample label="with actions">
+          <SubNav blue>
+            <SubNavTitle href="#">Fields</SubNavTitle>
+            <SubNavList>
+              <SubNavLink active href="#">
+                Glens
+              </SubNavLink>
+              <SubNavLink href="#">Dales</SubNavLink>
+              <SubNavLink href="#">Meadows</SubNavLink>
+            </SubNavList>
+            <SubNavActions>
+              <Button white small>
+                New Field
+              </Button>
+            </SubNavActions>
+          </SubNav>
+        </GuideExample>
+        <GuideExample label="no nav - with actions">
+          <SubNav blue>
+            <SubNavTitle href="#">Fields</SubNavTitle>
+            <SubNavActions>
+              <Button white small>
+                New Field
+              </Button>
+            </SubNavActions>
+          </SubNav>
+        </GuideExample>
+      </div>
+    ))
+  )
+  .add(
+    'Background Image',
+    withInfo(doc)(() => (
+      <div>
+        <GuideExample label="backgroundImage">
+          <SubNav
+            backgroundImage={greenBlueBg}
+            gradientFromColor="#70be49"
+            gradientToColor="#1688aa"
+          >
+            <SubNavTitle href="#" style={{ color: '#fff' }}>
+              Fields
+            </SubNavTitle>
+            <SubNavList>
+              <SubNavLink active href="#">
+                Glens
+              </SubNavLink>
+              <SubNavLink href="#">Dales</SubNavLink>
+              <SubNavLink href="#">Meadows</SubNavLink>
+            </SubNavList>
+          </SubNav>
+        </GuideExample>
+      </div>
+    ))
+  );
