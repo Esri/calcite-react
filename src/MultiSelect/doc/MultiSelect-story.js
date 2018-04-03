9f39926690e33f76d2667a2e21592df26d858353
Cody Lawson <CLawson@esri.com>
2018-04-03 11:39:04 -0600

    Add multiselect sample with custom renderer

--- src/MultiSelect/doc/MultiSelect-story.js
+++ src/MultiSelect/doc/MultiSelect-story.js
@@ -8,40 +8,92 @@ import doc from './MultiSelect.md';
 import { MenuItem } from '../../Menu';
 import MultiSelect from '../';
 
-storiesOf('MultiSelect', module).add(
-  'Controlled MultiSelect',
-  withInfo(doc)(() => {
-    class ControlledMultiSelect extends Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          selectedValues: [10]
+storiesOf('MultiSelect', module)
+  .add(
+    'Controlled MultiSelect',
+    withInfo(doc)(() => {
+      class ControlledMultiSelect extends Component {
+        constructor(props) {
+          super(props);
+          this.state = {
+            selectedValues: [10]
+          };
+        }
+
+        handleMultiSelectChange = (values, items) => {
+          this.setState({
+            selectedValues: values
+          });
         };
-      }
 
-      handleMultiSelectChange = (values, items) => {
-        this.setState({
-          selectedValues: values
-        });
-      };
-
-      render() {
-        return (
-          <div>
-            <GuideExample label="selectedItem={this.state.selectedValues}">
-              <MultiSelect
-                onChange={this.handleMultiSelectChange}
-                selectedValues={this.state.selectedValues}
-              >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </MultiSelect>
-            </GuideExample>
-          </div>
-        );
+        render() {
+          return (
+            <div>
+              <GuideExample label="selectedItem={this.state.selectedValues}">
+                <MultiSelect
+                  onChange={this.handleMultiSelectChange}
+                  selectedValues={this.state.selectedValues}
+                >
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </MultiSelect>
+              </GuideExample>
+            </div>
+          );
+        }
       }
-    }
-    return <ControlledMultiSelect />;
-  })
-);
+      return <ControlledMultiSelect />;
+    })
+  )
+  .add(
+    'Custom Label Renderer',
+    withInfo(doc)(() => {
+      class CustomLabelMultiSelect extends Component {
+        constructor(props) {
+          super(props);
+          this.state = {
+            selectedValues: [10]
+          };
+        }
+
+        handleMultiSelectChange = (values, items) => {
+          this.setState({
+            selectedValues: values
+          });
+        };
+
+        renderValue = items => {
+          if (!items || items.length === 0) {
+            return 'Select an Option';
+          } else if (items.length > 2) {
+            return `${items.length} Items Selected`;
+          } else {
+            return items.map(item => item.props.children).join(', ');
+          }
+        };
+
+        render() {
+          return (
+            <div>
+              <GuideExample label="selectedItem={this.state.selectedValues}">
+                <MultiSelect
+                  renderValue={this.renderValue}
+                  onChange={this.handleMultiSelectChange}
+                  selectedValues={this.state.selectedValues}
+                >
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                  <MenuItem value={40}>Fourty</MenuItem>
+                  <MenuItem value={50}>Fifty</MenuItem>
+                  <MenuItem value={60}>Sixty</MenuItem>
+                </MultiSelect>
+              </GuideExample>
+            </div>
+          );
+        }
+      }
+      return <CustomLabelMultiSelect />;
+    })
+  );
