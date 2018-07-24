3cd631c79ad402e13b7eed8780416845b523320d
Cody Lawson <CLawson@esri.com>
2018-07-24 15:49:32 -0600

    Date picker appendToBody example story

--- src/DatePicker/docs/DatePicker-story.js
+++ src/DatePicker/docs/DatePicker-story.js
@@ -111,4 +111,54 @@ storiesOf('DatePicker', module)
       };
       return <DatePickerStory />;
     })
+  )
+  .add(
+    'Append To Body',
+    withInfo({
+      text: doc,
+      propTables: [DatePicker]
+    })(() => {
+      class DatePickerStory extends Component {
+        constructor(props) {
+          super(props);
+          this.state = {
+            date: null,
+            datePickerFocused: false
+          };
+        }
+
+        onDateChange = date => {
+          this.setState({
+            date
+          });
+        };
+
+        onFocusChange = ({ focused }) => {
+          this.setState({
+            datePickerFocused: focused
+          });
+        };
+
+        render() {
+          return (
+            <div>
+              <GuideExample label="appendToBody" style={{ overflow: 'hidden' }}>
+                <DatePicker
+                  date={this.state.date}
+                  onDateChange={this.onDateChange}
+                  focused={this.state.datePickerFocused}
+                  onFocusChange={this.onFocusChange}
+                  appendToBody
+                />
+              </GuideExample>
+            </div>
+          );
+        }
+      }
+
+      DatePickerStory.propTypes = {
+        isStory: PropTypes.bool
+      };
+      return <DatePickerStory />;
+    })
   );
