7688da35882b68cf903800968bb63d103c53301e
Cody Lawson <CLawson@esri.com>
2018-12-03 12:49:52 -0700

    Remove withRefs

--- src/utils/withRefs.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const withRefs = Component => {
-  return React.forwardRef((props, ref) => {
-    return <Component forwardedRef={ref || props.forwardedRef} {...props} />;
-  });
-};
-
-export default withRefs;
