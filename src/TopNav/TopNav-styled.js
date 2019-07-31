3010e312a70e97e599600a35a3368d0e3586e3d8
Cody Lawson <CLawson@esri.com>
2019-07-31 12:29:00 -0600

    Fix active class style

--- src/TopNav/TopNav-styled.js
+++ src/TopNav/TopNav-styled.js
@@ -79,7 +79,7 @@ const StyledTopNavLink = styled(CalciteA)`
       border-bottom-color: ${props.theme.palette.blue};
     `};
 
-  .active,
+  &.active,
   .active > & {
     color: ${props => props.theme.palette.offBlack};
     border-bottom-color: ${props => props.theme.palette.blue};
