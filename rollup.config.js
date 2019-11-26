ef36c692a1bd5a5b492c4262efc2ae0ef694ed6d
John Gibson <dev@johngibson.me>
2019-11-26 09:11:43 -0800

    Create initial rollup config

---
+++ rollup.config.js
@@ -0,0 +1,57 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import multiInput from 'rollup-plugin-multi-input';
+import embedCSS from 'rollup-plugin-embed-css';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  // use glob in the input
+  input: ['src/**/index.js'],
+  output: {
+    format: 'esm',
+    dir: 'dist'
+  },
+  plugins: [
+    resolve({
+      preferBuiltins: true, // use local modules over matching builtin ones
+      mainFields: ['main', 'module']
+    }),
+    multiInput({ exports: false }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        react: [
+          'Children',
+          'Component',
+          'PureComponent',
+          'Fragment',
+          'createElement',
+          'createContext',
+          'forwardRef',
+          'useContext',
+          'useDebugValue',
+          'useEffect',
+          'useMemo',
+          'useState'
+        ],
+        'react-dom': ['render', 'findDOMNode'],
+        'prop-types': ['PropTypes'],
+        'react-is': [
+          'isForwardRef',
+          'isElement',
+          'isValidElementType',
+          'ForwardRef'
+        ],
+        'react-dates': ['SingleDatePicker', 'DateRangePicker'],
+        'react-popper': ['Manager', 'Reference', 'Popper'],
+        'react-virtualized': ['List']
+      }
+    }),
+    embedCSS(),
+    babel({
+      exclude: ['node_modules/**']
+    }),
+    terser()
+  ]
+};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+export {
+  default as Accordion,
+  AccordionSection,
+  AccordionContent,
+  AccordionTitle
+} from './Accordion';
+export { default as Button } from './Button';
